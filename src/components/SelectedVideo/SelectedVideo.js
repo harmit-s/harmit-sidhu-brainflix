d20cbb75700071d7f0b5420e4b4809a2779f5ef8
Harmit Sidhu <harmit_sidhu@outlook.com>
2024-04-08 01:57:11 -0400

    finished tablet styling

--- src/components/SelectedVideo/SelectedVideo.js
+++ src/components/SelectedVideo/SelectedVideo.js
@@ -14,13 +14,15 @@ const SelectedVideo = ({ video, title, channel, timestamp, views, likes, descrip
                 <video className="selected-video__video" poster={videoPoster} controls>{video}</video>
                 <div className='selected-video__container'>
                     <h2 className='selected-video__title'>{title}</h2>
-                    <div className='selected-video__layout'>
-                        <p className='selected-video__channel'>By {channel}</p>
-                        <div className='selected-video__views-layout'><img className='selected-video__views-icon' src={viewsIcon} alt='views icon' /> <p className='selected-video__views'>{views}</p></div>
-                    </div>
-                    <div className='selected-video__layout-2'>
-                        <p className='selected-video__timestamp'>{new Date(timestamp).toLocaleDateString()}</p>
-                        <div className='selected-video__likes-layout'><img className='selected-video__likes-icon' src={likesIcon} alt='likes icon' /> <p className='selected-video__likes'>{likes}</p></div>
+                    <div className='selected-video__tablet-view'>
+                        <div className='selected-video__layout'>
+                            <p className='selected-video__channel'>By {channel}</p>
+                            <div className='selected-video__views-layout'><img className='selected-video__views-icon' src={viewsIcon} alt='views icon' /> <p className='selected-video__views'>{views}</p></div>
+                        </div>
+                        <div className='selected-video__layout-2'>
+                            <p className='selected-video__timestamp'>{new Date(timestamp).toLocaleDateString()}</p>
+                            <div className='selected-video__likes-layout'><img className='selected-video__likes-icon' src={likesIcon} alt='likes icon' /> <p className='selected-video__likes'>{likes}</p></div>
+                        </div>
                     </div>
                     <p className='selected-video__description'>{description}</p>
 
@@ -29,8 +31,10 @@ const SelectedVideo = ({ video, title, channel, timestamp, views, likes, descrip
                         <div className="selected-video__layout-avatar">
                             <img src={avatar} className='selected-video__avatar' alt='avatar' />
                             <form className="selected-video__form">
-                                <label className="selected-video__subtitle">JOIN THE CONVERSATION</label>
-                                <textarea name="comment" type='text' className="selected-video__comment-input" placeholder="Add a new comment" required></textarea>
+                                <div className='selected-video__form-box'>
+                                    <label className="selected-video__subtitle">JOIN THE CONVERSATION</label>
+                                    <textarea name="comment" type='text' className="selected-video__comment-input" placeholder="Add a new comment" required></textarea>
+                                </div>
                                 <button className="selected-video__button"><img src={commentIcon} alt='comment icon' className='selected-video__icon' />COMMENT</button>
                             </form>
                         </div>
