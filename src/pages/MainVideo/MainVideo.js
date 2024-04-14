fffc39327630b4296831b531c4f8d833e615b296
Harmit Sidhu <harmit_sidhu@outlook.com>
2024-04-14 03:35:42 -0400

    final edits with feedback and functionality checks

--- src/pages/MainVideo/MainVideo.js
+++ src/pages/MainVideo/MainVideo.js
@@ -14,14 +14,14 @@ const MainVideoPage = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-       
+
         if (videoId) {
           const mainVideoResponse = await axios.get(
             `https://unit-3-project-api-0a5620414506.herokuapp.com/videos/${videoId}?api_key=f5a503e0-f571-43cd-9ec6-b29be74dce24`
           );
           setMainVideo(mainVideoResponse.data);
 
-         
+
           const videoListResponse = await axios.get(
             "https://unit-3-project-api-0a5620414506.herokuapp.com/videos?api_key=f5a503e0-f571-43cd-9ec6-b29be74dce24"
           );
@@ -30,13 +30,13 @@ const MainVideoPage = () => {
           );
           setVideoList(filteredVideoList);
         } else {
-        
+
           const defaultVideoResponse = await axios.get(
             "https://unit-3-project-api-0a5620414506.herokuapp.com/videos/84e96018-4022-434e-80bf-000ce4cd12b8?api_key=f5a503e0-f571-43cd-9ec6-b29be74dce24"
           );
           setMainVideo(defaultVideoResponse.data);
 
-          
+
           const videoListResponse = await axios.get(
             "https://unit-3-project-api-0a5620414506.herokuapp.com/videos?api_key=f5a503e0-f571-43cd-9ec6-b29be74dce24"
           );
@@ -56,7 +56,7 @@ const MainVideoPage = () => {
   return (
     <div className="main-video-page">
       {mainVideo && (
-      <video className="hero" poster={mainVideo.image} controls><source src={mainVideo.video} /></video>
+        <video className="hero" poster={mainVideo.image} controls><source src={mainVideo.video} /></video>
       )}
       <div className="main-video-page__layout">
         {mainVideo && <SelectedVideo video={mainVideo} />}
